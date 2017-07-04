871114c6319058e77b00546b0e764f345b2b9ad8
Anup Deb <debovik01@gmail.com>
2017-07-04 19:06:13 -0400

    Add files via upload

---
+++ CHIP8 EMULATOR/CHIP8/Chip.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CHIP8
+{
+    class Chip
+    {
+        // Initialization of memory
+        private ushort[] memory = new ushort[4096], V = new ushort[16], stack = new ushort[16];
+        private byte[] keys = new byte[16];
+        public byte[] display = new byte[64 * 32];
+        private ushort I = 0x0, pc = 0x200;
+        public int stackpointer = 0, sound_timer = 0, delay_timer = 0;
+
+        public bool needRedraw = false;
+        public ushort keyPressed;
+
+        int[] font =
+        {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+        };
+
+        // For opcodes involving random numbers
+        private Random rand = new Random();
+
+        public void run()
+        {
+            // Fetch opcode
+            ushort opcode = (ushort)((memory[pc] << 8) | memory[pc + 1]);
+
+            // Decode opcode
+            switch (opcode & 0xF000)
+            {
+                // Switch through first bit
+                case 0x0000:
+                    switch (opcode & 0x00FF)
+                    {
+                        case 0x00E0: //00E0	Clears the screen.
+                            for (int i = 0; i < display.Length; i++)
+                            {
+                                display[i] = 0;
+                            }
+                            pc += 0x02;
+                            needRedraw = true;
+                            break;
+
+                        case 0x00EE: //00EE	Returns from a subroutine.
+                            if (stackpointer > 0)
+                                stackpointer--;
+
+                            pc = (ushort)(stack[stackpointer] + 2);
+                            break;
+
+                        default:
+                            Debug.WriteLine("Unsupported Opcode!");
+                            System.Windows.Forms.Application.Exit();
+                            break;
+                    }
+                    break;
+
+                case 0x1000: //1NNN	Jumps to address NNN.
+                    pc = (ushort)(opcode & 0xFFF);
+                    break;
+
+                case 0x2000: //2NNN	Calls subroutine at NNN.
+                    stack[stackpointer++] = pc;
+                    pc = (ushort)(opcode & 0x0FFF);
+                    break;
+
+                case 0x3000: //3XNN	Skips the next instruction if VX equals NN.
+                    if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+                    {
+                        //skip next instruction
+                        pc += 0x04;
+                    }
+                    else
+                    {
+                        pc += 0x02;
+                    }
+                    break;
+
+                case 0x4000: //4XNN	Skips the next instruction if VX doesn't equal NN.
+                    if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+                    {
+                        //skip next instruction
+                        pc += 0x04;
+                    }
+                    else
+                    {
+                        pc += 0x02;
+                    }
+                    break;
+
+                case 0x5000: //5XY0	Skips the next instruction if VX equals VY.
+                    if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00F0) >> 4)
+                    {
+                        // Skip next instruction
+                        pc += 0x04;
+                    }
+                    else
+                    {
+                        pc += 0x02;
+                    }
+                    break;
+
+                case 0x6000: //6XNN	Sets VX to NN.
+                    V[(opcode & 0x0F00) >> 8] = (ushort)(opcode & 0x00FF);
+                    pc += 0x02;
+                    break;
+
+                case 0x7000: //7XNN	Adds NN to VX.
+                    V[(opcode & 0x0F00) >> 8] += (ushort)(opcode & 0x00FF);
+                    pc += 0x02;
+                    break;
+
+                case 0x8000:
+                    // Switch through last bit
+                    {
+                        int VX = V[(opcode & 0x0F00) >> 8], VY = V[(opcode & 0x00F0) >> 4];
+                        switch (opcode & 0x000F)
+                        {
+                            case 0x0000: //8XY0	Sets VX to the value of VY.
+                                V[(opcode & 0x0F00) >> 8] = (ushort)VY;
+                                pc += 0x02;
+                                break;
+
+                            case 0x0001: //8XY1	Sets VX to VX or VY.
+                                V[(opcode & 0x0F00) >> 8] = (ushort)(VX | VY);
+                                pc += 0x02;
+                                break;
+
+                            case 0x0002: //8XY2	Sets VX to VX and VY.
+                                V[(opcode & 0x0F00) >> 8] = (ushort)(VX & VY);
+                                pc += 0x02;
+                                break;
+
+                            case 0x0003: //8XY3	Sets VX to VX xor VY.
+                                V[(opcode & 0x0F00) >> 8] = (ushort)(VX ^ VY);
+                                pc += 0x02;
+                                break;
+
+                            case 0x0004: //8XY4	Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+                                // Check for carry
+                                if (VX > 0xFF - VY)
+                                {
+                                    V[0xF] = 1;
+                                }
+                                else
+                                {
+                                    V[0xF] = 0;
+                                }
+                                V[(opcode & 0x0F00) >> 8] = (ushort)((VX + VY) & 0xFF);
+                                pc += 0x02;
+                                break;
+
+                            case 0x0005: //8XY5	VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+                                if (VX > VY)
+                                {
+                                    V[0xF] = 1;
+                                }
+                                else
+                                {
+                                    V[0xF] = 0;
+                                }
+                                V[(opcode & 0x0F00) >> 8] = (ushort)((VX - VY) & 0xFF);
+                                pc += 0x02;
+                                break;
+
+                            case 0x0006: //8XY6	Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
+                                V[0xF] = (ushort)(V[(opcode & 0x0F00) >> 8] & 0x1);
+                                V[(opcode & 0x0F00) >> 8] = (ushort)(VX >> 1);
+                                pc += 0x02;
+                                break;
+
+                            case 0x0007: //8XY7	Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+                                if (VX > VY)
+                                {
+                                    V[0xF] = 0;
+                                }
+                                else
+                                {
+                                    V[0xF] = 1;
+                                }
+
+                                V[(opcode & 0x0F00) >> 8] = (ushort)((VY - VX) & 0xFF);
+                                pc += 0x02;
+                                break;
+
+                            case 0x000E: //8XYE	Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.
+                                V[0xF] = (ushort)(VX & 0x8);
+                                V[(opcode & 0x0F00) >> 8] = (ushort)(VX << 1);
+                                pc += 0x02;
+                                break;
+
+                            default:
+                                Debug.WriteLine("Unsupported Opcode!");
+                                System.Windows.Forms.Application.Exit();
+                                break;
+                        }
+                    }
+                    break;
+
+                case 0x9000: //9XY0	Skips the next instruction if VX doesn't equal VY.
+                    if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
+                    {
+                        pc += 0x04;
+                    }
+                    else
+                    {
+                        pc += 0x02;
+                    }
+                    break;
+
+                case 0xA000: //ANNN	Sets I to the address NNN.
+                    I = (ushort)(opcode & 0x0FFF);
+                    pc += 0x02;
+                    break;
+
+                case 0xB000: //BNNN Sets PC to the address NNN + V0.
+                    pc = (ushort)((opcode & 0x0FFF) + V[0]);
+                    break;
+
+                case 0xC000: //CXNN	Sets VX to the result of a bitwise and operation on a random number and NN.
+                    V[(opcode & 0x0F00) >> 8] = (ushort)(rand.Next(0, 0xFF + 1) & (opcode & 0x00FF));
+                    pc += 0x02;
+                    break;
+
+                case 0xD000: //DXYN: Draw a sprite (VX, VY) size (8, N). Sprite is located at I.
+                    V[0xF] = 0x0;
+                    {
+                        ushort height = (ushort)(opcode & 0x000F), pixel;
+                        int VX = V[(opcode & 0x0F00) >> 8], VY = V[(opcode & 0x00F0) >> 4];
+
+                        V[0xF] = 0;
+                        for (int y_pos = 0; y_pos < height; y_pos++)
+                        {
+                            pixel = memory[I + y_pos];
+                            for (int x_pos = 0; x_pos < 8; x_pos++)
+                            {
+                                if ((pixel & (0x80 >> x_pos)) != 0)
+                                {
+                                    try
+                                    {
+                                        if (display[(VX + x_pos + ((VY + y_pos) * 64)) % display.Length] == 1)
+                                        {
+                                            V[0xF] = 1;
+                                        }
+                                        display[VX + x_pos + ((VY + y_pos) * 64) % display.Length] ^= 1;
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Debug.WriteLine(e.ToString());
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    needRedraw = true;
+                    pc += 0x02;
+                    break;
+
+                case 0xE000:
+                    {
+                        int VX = V[(opcode & 0x0F00) >> 8];
+                        switch (opcode & 0x00FF)
+                        {
+                            case 0x009E: //EX9E	Skips the next instruction if the key stored in VX is pressed.
+                                if (VX == keyPressed)
+                                {
+                                    pc += 0x04;
+                                }
+                                else
+                                {
+                                    pc += 0x02;
+                                }
+                                break;
+
+                            case 0x00A1: //EXA1	Skips the next instruction if the key stored in VX isn't pressed.
+                                if (VX != keyPressed)
+                                {
+                                    pc += 0x04;
+                                }
+                                else
+                                {
+                                    pc += 0x02;
+                                }
+                                break;
+                        }
+                    }
+                    break;
+
+                case 0xF000:
+                    {
+                        int VX = V[(opcode & 0x0F00) >> 8];
+                        switch (opcode & 0x00FF)
+                        {
+                            case 0x0007: //FX07	Sets VX to the value of the delay timer.
+                                V[(opcode & 0x0F00) >> 8] = (ushort)delay_timer;
+                                pc += 0x02;
+                                break;
+
+                            case 0x000A: //FX0A	A key press is awaited, and then stored in VX.
+                                V[(opcode & 0x0F00) >> 8] = keyPressed;
+                                pc += 0x02;
+                                break;
+
+                            case 0x0015: //FX15	Sets the delay timer to VX.
+                                delay_timer = VX;
+                                pc += 0x02;
+                                break;
+
+                            case 0x0018: //FX18	Sets the sound timer to VX.
+                                sound_timer = VX;
+                                pc += 0x02;
+                                break;
+
+                            case 0x001E: //FX1E Adds VX to I.
+                                I += (ushort)VX;
+                                pc += 0x02;
+                                break;
+
+                            case 0x0029: //FX29	Sets I to the location of the sprite for the ushortacter in VX. ushortacters 0-F (in hexadecimal) are represented by a 4x5 font.
+                                I = (ushort)(0x050 + (VX * 5));
+                                pc += 0x02;
+                                break;
+
+                            case 0x0033: //FX33	Stores the MSB of VX at the address stored in I, middle digit at I + 1, and LSB at I + 2.
+                                memory[I] = (ushort)((VX - (VX % 100)) / 100);
+                                memory[I + 1] = (ushort)((-(VX % 100)) - (VX % 100 % 10) / 10);
+                                memory[I + 2] = (ushort)(VX - (-(VX % 100)) - (VX % 100 % 10));
+                                pc += 0x02;
+                                break;
+
+                            case 0x0055: //FX55	Stores V0 to VX in memory starting at address I.
+                                for (int i = 0; i <= VX; i++)
+                                {
+                                    memory[I + i] = V[i];
+                                }
+                                pc += 0x02;
+                                break;
+
+                            case 0x0065: //FX65	Fills V0 to VX with values from memory starting at address I.
+                                for (int i = 0; i <= VX; i++)
+                                {
+                                    V[i] = memory[I + i];
+                                }
+                                pc += 0x02;
+                                break;
+
+                            default:
+                                Debug.WriteLine("Unsupported Opcode!");
+                                System.Windows.Forms.Application.Exit();
+                                break;
+                        }
+                    }
+                    break;
+                default:
+                    Debug.WriteLine("Unsupported Opcode!");
+                    System.Windows.Forms.Application.Exit();
+                    break;
+            }
+            if (delay_timer > 0)
+                delay_timer--;
+
+            if (sound_timer > 0)
+            {
+                sound_timer--;
+            }
+        }
+
+        public void loadProgram(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                byte[] program = File.ReadAllBytes(filename);
+                for (int i = 0; i < program.Length; i++)
+                {
+                    memory[0x200 + i] = (ushort)(program[i] & 0xFF);
+                  
+                }
+            }
+            else
+            {
+                Debug.WriteLine("FILE NOT FOUND");
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        public void loadFont()
+        {
+            // Loads font into memory to be displayed later
+            for (int i = 0; i < font.Length; i++)
+            {
+                memory[0x50 + i] = (char)(font[i] & 0xFF);
+            }
+        }
+    }
+}
---
+++ CHIP8 EMULATOR/CHIP8/Display.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CHIP8
+{
+    public partial class Display : Form
+    {
+        // Init chip
+        static Chip c = new Chip();
+
+        // Threads
+        Thread chipThread, soundThread;// = new Thread(audioWorker);
+
+
+        // REFRESHRATE measured in Hz
+        const int REFRESHRATE = 60 * 10, SCALE = 10;
+
+        public Display()
+        {
+            InitializeComponent();
+
+            KeyPreview = true;
+
+            // Load the program into the chip
+            c.loadFont();
+
+            // Let the user choose their file
+            using (OpenFileDialog fileChooserDialog = new OpenFileDialog())
+            {
+                if (fileChooserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    c.loadProgram(fileChooserDialog.FileName);
+                }
+            }
+
+            //@"C:\Users\Nico\Documents\Visual Studio 2015\Projects\CHIP8\GAMES\pong.ch8");
+
+            // Init for window
+            displayGrid.Image = new Bitmap(displayGrid.Width, displayGrid.Height);
+
+            chipThread = new Thread(chipWorker);
+            soundThread = new Thread(soundWorker);
+        }
+
+        public void display()
+        {
+            try
+            {
+                Graphics g = Graphics.FromImage(displayGrid.Image);
+                // Draw black background
+                g.Clear(Color.Black);
+
+                for (int y = 0; y < 32; y++)
+                {
+                    for (int x = 0; x < 64; x++)
+                    {
+                        // Fill the rectangle based on value in the pixel array
+                        g.FillRectangle((c.display[(x + 64 * y)] == 1) ? Brushes.White : Brushes.Black, new Rectangle(x * SCALE, y * SCALE, SCALE, SCALE));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            displayGrid.Invalidate();
+        }
+
+        private void chipWorker()
+        {
+            // Variables for reference within loop
+            double msPerFrame = 1000.0 / REFRESHRATE;
+            DateTime frameStart, frameEnd;
+            TimeSpan ts;
+
+            // Infinite loop for the chip to run on
+            for (;;)
+            {
+                frameStart = DateTime.Now;
+                frameEnd = frameStart.AddMilliseconds(msPerFrame);
+
+                c.run();
+                if (c.needRedraw)
+                {
+                    display();
+                    c.needRedraw = false;
+                }
+
+                ts = frameEnd.Subtract(frameStart);
+                if (ts.Milliseconds > 0.0)
+                {
+                    Thread.Sleep(ts.Milliseconds); // Can be changed to determine speed
+                }
+            }
+        }
+
+        private void soundWorker()
+        {
+            // Infinite loop for the audio worker to run on
+            for (;;)
+            {
+                if (c.sound_timer > 0)
+                {
+                    // TODO need to modify this, it stops the game for a bit
+                    Console.Beep(2000, 17);
+                }
+            }
+        }
+
+        private void Display_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        // Keypress events
+        private void Display_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Map the key to chip8
+            c.keyPressed = (ushort)"1234qwerasdfzxcv".IndexOf(e.KeyChar);
+        }
+
+        private void Display_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Reset the keyPressed
+            c.keyPressed = 0;
+        }
+
+
+        // GUI events
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if(chipThread.IsAlive)
+            {
+                chipThread.Resume();
+                soundThread.Resume();
+            }
+            else
+            {
+                chipThread.Start();
+                soundThread.Start();
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            chipThread.Suspend();
+            soundThread.Suspend();
+        }
+
+        private void stepButton_Click(object sender, EventArgs e)
+        {
+            // TODO
+        }
+    }
+}
